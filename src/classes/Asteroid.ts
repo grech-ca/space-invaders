46c6f6adfdf53e21461cb913081c733667ff8e18
Mikhail Grechka <grech.code@gmail.com>
2024-04-27 12:36:10 +0400

    Make Asteroid rotate

--- src/classes/Asteroid.ts
+++ src/classes/Asteroid.ts
@@ -5,6 +5,8 @@ import { Player } from "./Player";
 const MAX_SIZE = 75
 
 export class Asteroid extends Entity {
+  rotationDirection = Math.round(Math.random()) ? 1 : -1
+
   constructor(data: {position: Position}) {
     const size = Math.round(Math.random() * 5) * 15
 
@@ -20,6 +22,7 @@ export class Asteroid extends Entity {
 
   tick() {
     this.position.y += 50 - Math.round(MAX_SIZE * 0.7) * this.size.height / MAX_SIZE
+    this.angle += (10 - 9 * this.size.height / MAX_SIZE) * this.rotationDirection
 
     this.level?.entities.forEach(player => {
       if (!(player instanceof Player)) return
